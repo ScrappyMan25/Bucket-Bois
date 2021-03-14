432c1f338e799ab46ef2c0cbbbda65bfe2b9fc9c
ScrappyMan25 <53944571+ScrappyMan25@users.noreply.github.com>
2021-03-14 16:37:40 +0000

    Tree Template Added
    -Tree temp added
    - Player collision added
    - Tree Mat. added

---
+++ Tower Defence Plant/Assets/Scripts/TreeScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    public int Health = 0;
+    public int rate_Of_Decay = 0;
+    public float AOE = 10f;
+    public SphereCollider Range;
+
+    private void OnEnable()
+    {
+        print("Started");
+        Range.radius = AOE;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Trigger");
+        print(other.ToString());
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        print("Trigger");
+        print(other.ToString());
+    }
+}
