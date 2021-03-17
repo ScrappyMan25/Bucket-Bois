1a39d52b5fe7c16a83b039c863398c6ef9bdc6cd
Wentong-Wu <80329793+Wentong-Wu@users.noreply.github.com>
2021-03-17 09:41:35 +0000

    EnemySearch

--- Tower Defence Plant/Assets/Scripts/Enemy.cs
+++ Tower Defence Plant/Assets/Scripts/Enemy.cs
@@ -4,18 +4,48 @@ public class Enemy : MonoBehaviour
 {
     public float speed = 10f;
     private Transform target;
-    private int waypointindex = 0;
+    private GameObject turret;
+    public int health;
 
-    void Start()
+    void Awake()
     {
-        //target = Waypoint.plants[0];
+        target = GameObject.Find("Cube").transform;
     }
 
     void Update()
     {
-        //Vector3 dir = target.position - transform.position;
-        //transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+        turret = FindClosestTurret();
+        float TargetDistance = Vector3.Distance(target.position, transform.position);       
+        float TurretDistance = Vector3.Distance(turret.transform.position, transform.position);
+        if (TargetDistance > TurretDistance)
+        {
+            Vector3 turretDir = turret.transform.position - transform.position;
+            transform.Translate(turretDir.normalized * speed * Time.deltaTime, Space.World);
+        }
+        else
+        {
+            Vector3 targetDir = target.position - transform.position;
+            transform.Translate(targetDir.normalized * speed * Time.deltaTime, Space.World);
+        }
+    }
 
-        //if target does not exist. Switch waypoint
+    public GameObject FindClosestTurret()
+    {
+        GameObject[] gos;
+        gos = GameObject.FindGameObjectsWithTag("Mother");
+        GameObject closest = null;
+        float distance = Mathf.Infinity;
+        Vector3 position = transform.position;
+        foreach(GameObject go in gos)
+        {
+            Vector3 diff = go.transform.position - position;
+            float curDistance = diff.sqrMagnitude;
+            if(curDistance < distance)
+            {
+                closest = go;
+                distance = curDistance;
+            }
+        }
+        return closest;
     }
 }
