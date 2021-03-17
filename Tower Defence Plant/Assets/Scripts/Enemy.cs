e2c17ccad49c48b850ad97488b7f0d29e2ee538e
Wentong-Wu <80329793+Wentong-Wu@users.noreply.github.com>
2021-03-17 13:21:22 +0000

    Updated Enemy Ai

--- Tower Defence Plant/Assets/Scripts/Enemy.cs
+++ Tower Defence Plant/Assets/Scripts/Enemy.cs
@@ -14,18 +14,14 @@ public class Enemy : MonoBehaviour
 
     void Update()
     {
-        turret = FindClosestTurret();
-        float TargetDistance = Vector3.Distance(target.position, transform.position);
-        float TurretDistance = Vector3.Distance(turret.transform.position, transform.position);
-        if (TargetDistance > TurretDistance)
-        {
-            Vector3 turretDir = turret.transform.position - transform.position;
-            transform.Translate(turretDir.normalized * speed * Time.deltaTime, Space.World);
-        }
-        else
+        try
         {
             Vector3 targetDir = target.position - transform.position;
             transform.Translate(targetDir.normalized * speed * Time.deltaTime, Space.World);
+        }
+        catch
+        {
+            target = GameObject.FindGameObjectWithTag("Mother").transform;
         }
     }
 
@@ -49,15 +45,13 @@ public class Enemy : MonoBehaviour
         return closest;
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerStay(Collider other)
     {
-        if (collision.gameObject.name == "Bullet")
-        {
-            health -= 10;
-        }
-        if (health <= 0)
-        {
-            Destroy(gameObject);
-        }
+        target = other.transform;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        target = GameObject.FindGameObjectWithTag("Mother").transform;
     }
 }
