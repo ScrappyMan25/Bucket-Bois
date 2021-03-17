0f3b2a7586a16bc90edb30f1436b0ca58bb7d295
PH20001 <80325931+PH20001@users.noreply.github.com>
2021-03-17 14:52:21 +0000

    Player properties and gun
    uploading to branch to merge with main

---
+++ Tower Defence Plant/Assets/Scripts/Bullet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 30f;
+    public float bulletLife = 3f;
+
+    private float bulletTimer;
+    void Start()
+    {
+        bulletTimer = bulletLife;
+    }
+
+    
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+
+        bulletTimer -= Time.deltaTime;
+        if(bulletTimer <= 0f)
+        {
+            Destroy(gameObject);
+        }
+
+        
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Enemy")
+        {
+           
+            Debug.Log("Hit Enemy");
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.layer == 8)
+        {
+            Destroy(gameObject);
+            Debug.Log("bullet hit ground");
+        }
+    }
+}
---
+++ Tower Defence Plant/Assets/Scripts/Gun.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public float ammoCount = 50f;
+
+    public GameObject bulletPrefab;
+    public Camera fpsCam;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            if (ammoCount > 0)
+            {
+                GameObject bulletObject = Instantiate(bulletPrefab);
+                bulletObject.transform.position = transform.position + transform.forward;
+                bulletObject.transform.forward = fpsCam.transform.forward;
+                ammoCount -= 1;
+            }
+        }
+    }
+
+
+    
+}
