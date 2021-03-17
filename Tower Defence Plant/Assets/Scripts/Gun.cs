d8a7dbdc91ca763257d891525510ce2fa57afe77
PH20001 <80325931+PH20001@users.noreply.github.com>
2021-03-17 18:09:33 +0000

    Update Gun.cs
    Added reload function to gun waits for 2 seconds then goes back to max ammo capacity

--- Tower Defence Plant/Assets/Scripts/Gun.cs
+++ Tower Defence Plant/Assets/Scripts/Gun.cs
@@ -1,10 +1,14 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class Gun : MonoBehaviour
 {
     public float damage = 10f;
     public float range = 100f;
+    public float TimeToReload = 2f;
     public float ammoCount = 50f;
+    public float maxAmmo = 50f;
+    private bool isReloading = false;
 
     public GameObject bulletPrefab;
     public Camera fpsCam;
@@ -14,6 +18,11 @@ public class Gun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if(isReloading)
+        {
+            return;
+        }
         if(Input.GetButtonDown("Fire1"))
         {
             if (ammoCount > 0)
@@ -24,6 +33,26 @@ public class Gun : MonoBehaviour
                 ammoCount -= 1;
             }
         }
+
+        if(ammoCount <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+    }
+
+
+    IEnumerator Reload ()
+    {
+        isReloading = true;
+        Debug.Log("reloading");
+
+        yield return new WaitForSeconds(TimeToReload);
+
+
+        ammoCount = maxAmmo;
+        isReloading = false;
+
     }
 
 
