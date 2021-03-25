25111a2fa13fc0a30ad143dc8ee8853dd7b86430
Wentong-Wu <80329793+Wentong-Wu@users.noreply.github.com>
2021-03-25 01:06:39 +0000

    CameraLimit

--- Scrappy AAnd WUMAN/BucketBoi/Scripts/LevelManager.gd
+++ Scrappy AAnd WUMAN/BucketBoi/Scripts/LevelManager.gd
@@ -18,7 +18,6 @@ func _ready() -> void:
 	add_child(temp)
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_key_pressed(KEY_R):
